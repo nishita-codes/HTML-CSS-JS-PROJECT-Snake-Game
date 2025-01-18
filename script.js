b01087994db88d1e60057e77ba699503f580bc5b
Nishita-codes <nishitapatidar03@gmail.com>
2025-01-18 23:26:19 +0530

    added files

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 console.log("welcome to my snake game");
+let direction ={x: 0 , y:0};
+let foodSound= new Audio('../music/food.mp3');
+let gameOverSound = new Audio('../music/gameover.mp3');
+let moveSound = new Audio('../music/move.mp3');
+let musicSound = new Audio('../music/music.mp3');
+
+//Game Function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Main logic starts here
+window.requestAnimationFrame(main);
