6f2bf7b8b999da0873a33adcaec8e819610302d7
Nishita-codes <nishitapatidar03@gmail.com>
2025-01-18 23:42:59 +0530

    added files

--- script.js
+++ script.js
@@ -4,10 +4,28 @@ let foodSound= new Audio('../music/food.mp3');
 let gameOverSound = new Audio('../music/gameover.mp3');
 let moveSound = new Audio('../music/move.mp3');
 let musicSound = new Audio('../music/music.mp3');
+let speed = 2;
+let lastPaintTime = 0;
+let snakeArr =[
+    {x: 13, y:15}
+]
 
 //Game Function
+function main(ctime) {
+    window.requestAnimationFrame(main);
+    console.log(ctime)
+    if((ctime - lastPaintTime)/1000 < 1/speed){
+        return;
+    }
+    lastPaintTime = ctime;
+    gameEngine();
+     
+}
+ function gameEngine(){
+    // part1:updating the snake array
 
-
+    // part2: render the snake and food
+}
 
 
 
